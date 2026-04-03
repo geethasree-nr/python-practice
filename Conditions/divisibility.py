41aa65717f4d0c9438747bc90a0cbac22de770a4
demo <geethasreenr@gmail.com>
2026-04-03 14:23:31 +0530

    Add conditions

---
+++ Conditions/divisibility.py
@@ -0,0 +1,7 @@
+num=int(input("Enter the number:"))
+
+if(num%2==0 and num%3==0):
+    print(f"{num} is divisible by both 2 and 3")
+else:
+    print(f"{num} is not divisible by both 2 and 3")
+    

00cfe2336e7f1a03d40b4b08b13fd5fead08c51a
demo <geethasreenr@gmail.com>
2026-04-04 09:27:45 +0530

    add sliding window

---
+++ Strings/longest_substring.py
@@ -0,0 +1,16 @@
+def longest_substring(s):
+    res_set=set()
+    result=0
+    l=0
+    for i in range(0,len(s)):
+        while s[i] in res_set:
+            res_set.remove(s[l])
+            l+=1
+        res_set.add(s[i])
+        result=max(result,i-l+1)
+    return result
+
+s="abcabcabb"
+print(longest_substring(s))
+
+

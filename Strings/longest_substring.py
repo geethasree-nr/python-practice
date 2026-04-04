2c9f297fbff78f4f9af56b225f17d038866fc8ad
demo <geethasreenr@gmail.com>
2026-04-04 09:40:06 +0530

    updated sliding window

--- Strings/longest_substring.py
+++ Strings/longest_substring.py
@@ -1,12 +1,12 @@
 def longest_substring(s):
-    res_set=set()
+    res_set=[]
     result=0
     l=0
     for i in range(0,len(s)):
         while s[i] in res_set:
             res_set.remove(s[l])
             l+=1
-        res_set.add(s[i])
+        res_set.append(s[i])
         result=max(result,i-l+1)
     return result
 

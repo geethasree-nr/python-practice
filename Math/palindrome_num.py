b049acec49e523b50f7ff9d16e8ae56e38bf22ab
demo <geethasreenr@gmail.com>
2026-04-13 12:11:34 +0530

    Added palindrome number

---
+++ Math/palindrome_num.py
@@ -0,0 +1,13 @@
+n=int(input("Enter the number: "))
+temp=n
+rev=0
+while n>0:
+    rem=n%10
+    rev=rev*10 + rem
+    n=n//10
+if temp==rev:
+    print("The number is a palindrome.")
+else:  
+     print("The number is not a palindrome.")
+
+

d6c4b6ee9c2ceddeb221f4dc740b1b3a899b0b0b
demo <geethasreenr@gmail.com>
2026-04-27 10:56:42 +0530

    Added bubble sort

---
+++ Sorting/bubble_sort.py
@@ -0,0 +1,7 @@
+nums=[5, 4, 3, 2, 1]
+for i in range(0,len(nums)):
+    for j in range(i+1,len(nums)):
+        if nums[i]>nums[j]:
+            nums[i],nums[j]=nums[j],nums[i]
+        print(nums)
+    
